f7983bf66259ced651a383ae4800b2b9c9671b5a
bindupriyachilukuri <72906262+bindupriyachilukuri@users.noreply.github.com>
2020-10-23 12:02:27 +0530

    Add files via upload

---
+++ sumseries2/src/module-info.java
@@ -0,0 +1,2 @@
+module sumseries2 {
+}
---
+++ sumseries2/src/sumseries2/sum_series_2.java
@@ -0,0 +1,23 @@
+package sumseries2;
+
+import java.util.Scanner;
+
+public class sum_series_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int i,n,sign=-1;
+		float sum=0;
+		n=sc.nextInt();
+
+		for(i=1;i<=n;i++)
+		{
+			sign *= -1;
+			sum += sign*1.0/i;
+		}
+		System.out.println(sum);
+	}
+	}
+
+
