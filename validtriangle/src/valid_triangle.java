072a5a8d5af1c807531bbcd08786a38cb4c24261
bindupriyachilukuri <72906262+bindupriyachilukuri@users.noreply.github.com>
2020-10-23 11:26:43 +0530

    Add files via upload

---
+++ validtriangle/src/valid_triangle.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class valid_triangle
+{
+public static void main(String []args)
+{
+int x;int y;int z;
+Scanner sc=new Scanner(System.in);
+x=sc.nextInt();
+y=sc.nextInt();
+z=sc.nextInt();
+if ((x+y+z)==180)
+{
+System.out.println("Valid");
+}
+else
+{
+System.out.println("Not Valid");
+}
+}
+}
+
