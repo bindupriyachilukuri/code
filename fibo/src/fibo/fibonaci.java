1fc69efdb40352baf6d0e0d88b53ad6653b41490
bindupriyachilukuri <72906262+bindupriyachilukuri@users.noreply.github.com>
2020-10-23 11:50:28 +0530

    Add files via upload

---
+++ fibo/src/fibo/fibonaci.java
@@ -0,0 +1,27 @@
+package fibo;
+
+import java.util.Scanner;
+
+public class fibonaci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int f1 = 0, f2 = 1, i,n; 
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if (n < 1) 
+		{
+		    return;
+		}   
+		for (i = 1; i <= n; i++) 
+	    { 
+		    System.out.print(f2+" "); 
+		    int next = f1 + f2; 
+		    f1 = f2; 
+		    f2 = next; 
+		 } 
+		        System.out.println(n);
+		}
+	}
+
+
